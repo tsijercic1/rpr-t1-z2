085eedc64215e83628c8e0302eb56c209b827250
Tarik Sijerčić <tsijercic1@etf.unsa.ba>
2019-10-14 14:19:57 +0200

    Ispravljena klasa Sat

--- src/ba/unsa/etf/rpr/tutorijal1/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal1/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Sat s = new Sat(1, 1, 1);
+        s.Ispisi();
+        s.Ispisi();
+        s.Sljedeci();
+        s.Ispisi();
+        s.PomjeriZa(-48);
+        s.Ispisi();
+        s.Postavi(0,0,0);
+        s.Ispisi();
+
+
     }
 }
